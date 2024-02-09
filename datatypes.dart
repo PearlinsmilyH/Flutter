a14b60275be91e348cf55ba64c294e83b5b04f9f
PearlinsmilyH <pearlinsmily.h@datasirpi.com>
2024-02-09 15:52:46 +0530

    Data Types in Dart

---
+++ datatypes.dart
@@ -0,0 +1,45 @@
+void main() {
+  // Numbers
+  int integerNumber = 42;
+  double doubleNumber = 3.14;
+
+  // Strings
+  String message = "My Name is Smily";
+  
+  // Boolean
+  bool isSoftwareEngineer = true;
+  bool isStudent = false;
+
+  // Lists
+  List<int> numbersList = [1, 2, 3, 4, 5];
+  List<String> fruitsList = ["Apple", "Banana", "Orange"];
+
+  // Maps
+  Map<String, dynamic> person = {
+    "name": "Smily",
+    "age": 25,
+    "isStudent": false,
+  };
+
+  // Dynamic
+  dynamic dynamicVariable = "I can be anything";
+
+  // Final
+  final int finalNumber = 100;
+
+  // Const
+  const double piValue = 3.14159;
+
+  // Printing values
+  print("Integer: $integerNumber");
+  print("Double: $doubleNumber");
+  print("String: $message");
+  print("Boolean (true): $isSoftwareEngineer");
+  print("Boolean (false): $isStudent");
+  print("List of numbers: $numbersList");
+  print("List of fruits: $fruitsList");
+  print("Map: $person");
+  print("Dynamic Variable: $dynamicVariable");
+  print("Final Number: $finalNumber");
+  print("Constant Pi Value: $piValue");
+}
