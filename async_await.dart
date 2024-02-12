58ae748beb01e6142c1bc9052bef32fd2d3c3ae5
PearlinsmilyH <157511754+PearlinsmilyH@users.noreply.github.com>
2024-02-12 17:43:30 +0530

    Update async_await.dart

--- async_await.dart
+++ async_await.dart
@@ -1,4 +1,4 @@
-void main() async {
+void main() async {                                      //the async keyword is used to denote that it will carry out some other work
   print('Start of program');
   await delayedPrint('Hello, ', Duration(seconds: 2));
   await delayedPrint('async ', Duration(seconds: 2));
