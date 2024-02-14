1a84e2288a8791879c5f46e74e8f5934410b5add
PearlinsmilyH <pearlinsmily.h@datasirpi.com>
2024-02-14 12:49:17 +0530

    scaffold

---
+++ lib/main.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(myApp());
+}
+
+class myApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(home: Scaffold());
+  }
+}
