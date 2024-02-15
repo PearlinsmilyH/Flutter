ecf36123036f7f0eb275f9ac166bb96e550a25be
PearlinsmilyH <pearlinsmily.h@datasirpi.com>
2024-02-15 12:48:42 +0530

    list_images

---
+++ lib/main.dart
@@ -0,0 +1,50 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Image ListView',
+      home: ImageListView(),
+    );
+  }
+}
+
+class ImageListView extends StatelessWidget {
+  final List<String> imagePaths = [
+    'assets/pinkflower.jpg',
+    'assets/redflower.jpeg',
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Image ListView'),
+      ),
+      body: ListView.builder(
+        itemCount: imagePaths.length,
+        itemBuilder: (_, index) {
+          return Expanded(
+            child: ListTile(
+              title: Image.asset(
+                imagePaths[index],
+                // width: 500,
+                // height: 500,
+                fit: BoxFit.cover,
+              ),
+              onTap: () {
+                // Handle image tap event
+                print('Tapped image ${index + 1}');
+              },
+            ),
+          );
+        },
+      ),
+    );
+  }
+}
