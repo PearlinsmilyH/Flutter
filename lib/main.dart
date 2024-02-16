eeaab1356b0d8c88b76829e374a144ffc8fd066b
PearlinsmilyH <pearlinsmily.h@datasirpi.com>
2024-02-16 15:45:49 +0530

    card

---
+++ lib/main.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+import 'page.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(title: Text('Card')),
+        backgroundColor: Colors.blueGrey,
+        body: MyCardWidget(),
+      ),
+    );
+  }
+}
---
+++ lib/page.dart
@@ -0,0 +1,42 @@
+import 'package:flutter/material.dart';
+
+class MyCardWidget extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Container(
+        width: 300,
+        height: 250,
+        padding: EdgeInsets.all(10.0),
+        child: Card(
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(15.0),
+          ),
+          color: const Color.fromARGB(255, 212, 54, 244),
+          elevation: 10,
+          child: ListView( // Wrap with ListView
+            children: [
+              ListTile(
+                leading: Icon(Icons.album, size: 60),
+                title: Text('Smily', style: TextStyle(fontSize: 30.0)),
+                subtitle: Text('Best of Smily Music.', style: TextStyle(fontSize: 18.0)),
+              ),
+              ButtonBar(
+                children: [
+                  ElevatedButton(
+                    child: Text('Play'),
+                    onPressed: () {},
+                  ),
+                  ElevatedButton(
+                    child: Text('Pause'),
+                    onPressed: () {},
+                  ),
+                ],
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
