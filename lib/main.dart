c6dc4fc0aa161ed57d9e324d0c840e00b14a25ea
PearlinsmilyH <157511754+PearlinsmilyH@users.noreply.github.com>
2024-02-25 22:05:09 +0530

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -14,8 +14,8 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
-  StreamController<String> streamController = StreamController(); //getting the instance of StreamController() in streamController
-  TextEditingController textEditingController = TextEditingController(); // used to control the text input field and retrieve its value.
+  StreamController<String> streamController = StreamController();               //getting the instance of StreamController() in streamController
+  TextEditingController textEditingController = TextEditingController();        // used to control the text input field and retrieve its value.
 
   @override
   Widget build(BuildContext context) {
@@ -30,10 +30,10 @@ class _MyAppState extends State<MyApp> {
           //column display its children in a vertical arrangement
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            StreamBuilder<String>(
-                stream: streamController.stream, //
+            StreamBuilder<String>(                                  //The StreamBuilder listens to changes in the data stream and updates the UI accordingly, showing                                                                  
+                  if (snapshot.hasData) {                           //"No data" if there's nothing entered yet or displaying the entered name.
+                stream: streamController.stream, 
                 builder: (context, snapshot) {
-                  if (snapshot.hasData) {
                     String output = snapshot.data.toString();
                     return Text(output,
                         style: TextStyle(fontSize: 15));
@@ -51,12 +51,12 @@ class _MyAppState extends State<MyApp> {
                         border: OutlineInputBorder(
                             borderRadius:
                                 BorderRadius.all(Radius.circular(5)))))),
-            SizedBox(height: 40.0), //space between the textbox and the button
+            SizedBox(height: 40.0),                                         //space between the textbox and the button
             ElevatedButton(
                 onPressed: () {
                   String text = textEditingController.text;
                   streamController
-                      .add(text); //this will add the text in the "no data" part
+                      .add(text);                                            //this will add the text in the "no data" part
                 },
                 child: Text("submit"))
           ],
@@ -65,3 +65,15 @@ class _MyAppState extends State<MyApp> {
     );
   }
 }
+
+/* creates with a text input field and a submit button.
+When we enter our name in the input field and press "submit," our name gets displayed 
+on the screen. This is done using a StreamController, which manages the data flow in the app.
+The StreamBuilder listens to changes in the data stream and updates the UI accordingly, showing 
+"No data" if there's nothing entered yet or displaying the entered name.*/
+
+
+
+
+
+
