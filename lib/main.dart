2d8f104de7ccb0c9c1bc00f9bc11b26d40274c0b
PearlinsmilyH <157511754+PearlinsmilyH@users.noreply.github.com>
2024-02-25 23:46:49 +0530

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,25 +1,26 @@
 import 'package:flutter/material.dart';
 
-void main() {
+void main() {                                                 //the "main()" function initializes the application by calling "runApp()"  with the root widget "MyApp"
   runApp(MyApp());
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatelessWidget {                        //indicating that this widget doesn't have any mutable state
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context) {                       //used for accessing inherited widgets
     const title = 'Basic List';
 
-    return MaterialApp(
+    return MaterialApp(                                       //MaterialApp is a widget that represents the root of your application
+                                                              //MaterialApp sets up various configurations for your app
       title: title,
       home: Scaffold(
-        appBar: AppBar(
+        appBar: AppBar(                                       
           title: const Text(
             title,
             style: TextStyle(color: Colors.blue, fontWeight: FontWeight.bold),
           ),
         ),
         body: ListView(
-          children: const <Widget>[
+          children: const <Widget>[                                  //that accept multiple child widgets
             ListTile(
               leading: Icon(Icons.email),
               title: Text('email'),
